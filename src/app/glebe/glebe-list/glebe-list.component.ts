b1025dcf421e56c3dffd638ddb1791ae1c8a3624
Gabriel Menin <gabriel.menin@aegro.com.br>
2022-06-02 22:40:42 -0300

    feat: get farm by path

--- src/app/glebe/glebe-list/glebe-list.component.ts
+++ src/app/glebe/glebe-list/glebe-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Farm } from 'src/app/interfaces/farm.model';
 import { Glebe } from 'src/app/interfaces/glebe.model';
+import { FarmService } from 'src/app/service/farm.service';
 import { GlebeService } from 'src/app/service/glebe.service';
 
 @Component({
@@ -11,15 +13,31 @@ import { GlebeService } from 'src/app/service/glebe.service';
 export class GlebeListComponent implements OnInit {
 
   farmId = this.activatedRoute.snapshot.params['id'];
+  farm: Farm | undefined;
   glebes: Glebe[] = [];
 
   constructor(
     private glebeService: GlebeService,
+    private farmService: FarmService,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.getFarm(this.farmId);
     this.getGlebes(this.farmId);
+  }
+
+  public getFarm(id: string): void {
+    if(id){
+      this.farmService.getFarmById(id).subscribe({
+        next: (responseData) => {
+          console.log(responseData);
+          this.farm = responseData;
+        },
+        error: (e) => console.error(e),
+        complete: () => console.log("Completed")
+      })
+    }
   }
 
   public getGlebes(farmId: string): void {
