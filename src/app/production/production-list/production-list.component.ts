823e4c235ab02fabe73c01ae44f17cf41c8e0cba
Gabriel Menin <gabriel.menin@aegro.com.br>
2022-06-03 16:14:20 -0300

    feat: get glebe by path

--- src/app/production/production-list/production-list.component.ts
+++ src/app/production/production-list/production-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Glebe } from 'src/app/interfaces/glebe.model';
 import { Production } from 'src/app/interfaces/production.model';
+import { GlebeService } from 'src/app/service/glebe.service';
 import { ProductionService } from 'src/app/service/production.service';
 
 @Component({
@@ -10,13 +13,33 @@ import { ProductionService } from 'src/app/service/production.service';
 export class ProductionListComponent implements OnInit {
 
   glebeId!: string;
+  glebe!: Glebe;
   productions: Production[] = [];
 
   constructor(
-    private productionService: ProductionService
+    private productionService: ProductionService,
+    private glebeService: GlebeService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.glebeId = this.activatedRoute.snapshot.params['id'];
+
+    this.getGlebe();
+    this.getProductions;
+  }
+
+  getGlebe(): void {
+    if(this.glebeId){
+      this.glebeService.getGlebeById(this.glebeId).subscribe({
+        next: (responseData) => {
+          console.log(responseData);
+          this.glebe = responseData;
+        },
+        error: (e) => console.error(e),
+        complete: () => console.log("Completed")
+      })
+    }
   }
 
   getProductions(): void {
